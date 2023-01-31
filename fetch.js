ed24a6acaa44d19a369f3e7080a4fa15d7fcc06b
Tringapps-Mohan <102800135+Tringapps-Mohan@users.noreply.github.com>
2023-01-31 19:39:41 +0530

    1 file modified

--- fetch.js
+++ fetch.js
@@ -1,45 +1,49 @@
-const url1 = "https://jsonplaceholder.typicode.com/todos/";
-const url2 = "https://www.boredapi.com/api/activity";
-const https = require("https");
-const fetch = (url)=>new Promise((resolve,reject)=>{
+const url1 = 'https://jsonplaceholder.typicode.com/todos/';
+const url2 = 'https://www.boredapi.com/api/activity';
+const https = require('https');
+const fetch = (url) =>
+  new Promise((resolve, reject) => {
     {
-        const request = https.get(url,(res)=>{
-            let data = "";
-            res.on("data",(chunk)=>{
-                data += chunk;
-            });
-    
-            res.on("end",()=>{
-                resolve(data);
-            });
+      const request = https.get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
         });
 
-        request.on("error",(error)=>{
-            reject(error);
+        res.on('end', () => {
+          resolve(data);
         });
+      });
 
-        request.end();
+      request.on('error', (error) => {
+        reject(error);
+      });
+
+      request.end();
     }
-});
-
+  });
 
 //let url = process.argv[0];
 
 // fetch(url2).then(res=>console.log(res,typeof(res))).catch(err=>console.log(err));
 
-module.exports = (req,res,next)=>{
-    let {api} = req.params;
-    let data;
-    switch(api){
-        case "bored":
-        fetch(url2).then(res=>res.json()).then(res=>data=res).catch(e=>data=e);
-        break;
-        case "json":
-            fetch(url2).then(res=>res.json()).then(res=>data=res).catch(e=>data=e);
-            break;
-        default:
-            data="undefined";
-    }
-    res.data = data;
+module.exports = (req, res, next) => {
+  let { api } = req.params;
+  const fun = async (url) => {
+    res.data = await fetch(url)
+      .then((res) => JSON.parse(res))
+      .catch((e) => e);
     next();
+  };
+
+  switch (api) {
+    case 'bored':
+      fun(url2);
+      break;
+    case 'json':
+      fun(url1);
+      break;
+    default:
+      res.data = 'undefined';
+  }
 };
